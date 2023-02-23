633ae383fcb9d38e4bb9a30577963cc200c47577
Andrey <Rikitikitime95@mail.ru>
2023-02-23 20:12:47 +0300

    add Variable in this project

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,17 @@
+// "comment" - ставить комменты
+// JDK - прога для работы java кода
+
 public class Main {
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        // типы данных:
+        byte num = 2;                                     // byte - небольшое число
+        int i = 123456;                                   // int - среднее число
+        long l = 324345645;                        // long - большое число
+        float num_2 = 123.123f;                  // float - числа с точкой
+        double num_3 = 123456.123456;   // double - с точкой, только диапозон значений в 2 раза больше чем у float
+        double num_4;  // переменная num_4
+        num_4 = 123;  // задаём переменную (чему равна переменная num_4)
+        System.out.print ("Variable = " + num_4);  // вывести переменную
     }
 }
