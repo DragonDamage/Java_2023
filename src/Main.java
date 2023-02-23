8f34e6d1d0b6ee5e71e415e24bf08829b0488346
Andrey <Rikitikitime95@mail.ru>
2023-02-23 22:00:03 +0300

    Change Main.java

--- src/Main.java
+++ src/Main.java
@@ -5,13 +5,21 @@ public class Main {
 
     public static void main(String[] args) {
         // типы данных:
-        byte num = 2;                                     // byte - небольшое число
-        int i = 123456;                                   // int - среднее число
-        long l = 324345645;                        // long - большое число
-        float num_2 = 123.123f;                  // float - числа с точкой
-        double num_3 = 123456.123456;   // double - с точкой, только диапозон значений в 2 раза больше чем у float
-        double num_4;  // переменная num_4
-        num_4 = 123;  // задаём переменную (чему равна переменная num_4)
-        System.out.print ("Variable = " + num_4);  // вывести переменную
+        byte num = 2;                                           // byte - небольшое число
+        int i = 123456;                                         // int - среднее число
+        long l = 324345645;                                // long - большое число
+        float num_2 = 123.123f;                        // float - числа с точкой
+        double num_3 = 123456.123456;          // double - с точкой, только диапозон значений в 2 раза больше чем у float
+        double num_4;                                   // переменная num_4
+        num_4 = 123;                                  // задаём переменную (чему равна переменная num_4)
+        char sym = 'A';                               // символ - можно только одну букву
+        boolean is_java = true;                   // задать значение либо "true" либо "false"
+        String str = "Hello World!";            // строка
+
+        // println - вывести на новой строке
+        System.out.println ("Variable = " + num_4);   // вывести переменную
+        System.out.println ("Variable = " + sym);
+        System.out.println ("Variable = " + is_java);
+        System.out.println ("Variable = " + str);
     }
 }
